b372c6985e3ac756fa5550b9cab6bc19a463e6cb
devlrakib <devrakib5@gmail.com>
2024-01-14 03:29:04 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+// var num =5;
+// var result =num*num;
+// document.write("Result =  "+result+"</br>")
+// var num =6;
+// var result =num*num;
+// document.write("Result =  "+result+"</br>")
+// var num =7;
+// var result =num*num;
+// document.write("Result =  "+result+"</br>")
+
+
+// Function Akta Template Jeikhane Multiple Time Use Kora Jabe
+
+
+// Creating Function
+function square (num){
+     var result =num*num;
+     document.write("Result =  "+result+"</br>")
+}
+
+// Calling Function
+square(5);
+square(6);
+square(8);
